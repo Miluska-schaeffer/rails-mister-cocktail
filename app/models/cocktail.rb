498fafe96d7fe1e3420eddd30099d5983f1948f7
miluska schaeffer <miluska.schaeffer@gmail.com>
2020-12-22 22:42:14 +0100

    show page layout fix

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -6,21 +6,10 @@ class Cocktail < ApplicationRecord
 
   CATEGORY =  ["Aromatic","Old Fashioned","Sour", "Highball", "Festive", "Non-Alcoholic" ]
 
-
   validates :name, presence: true, uniqueness: true, length: { in: 3..40 }
-  # validates :category, inclusion: { in: CATEGORY, message: "Not a valid category" }
+  validates :category, inclusion: { in: CATEGORY, message: "Not a valid category" }
   validates :content, presence: true, length: { in: 15..100 }
 
-
-include PgSearch::Model
-  pg_search_scope :search_by_cocktail_attributes,
-    against: [:name, :ingredient],
-    associated_against: {
-      dose: [:ingredient]
-    },
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
 
 
 end
